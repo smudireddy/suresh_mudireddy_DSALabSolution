8dccdec38fa63f3fad80b035b2dbfdfe0fc9d189
sumudire <sumudire@in.ibm.com>
2022-05-01 11:18:48 +0530

    DSA Lab Session 3 files formatted

--- src/com/gl/iitr/dsalab/a1/BracketsBalancer.java
+++ src/com/gl/iitr/dsalab/a1/BracketsBalancer.java
@@ -3,54 +3,48 @@ package com.gl.iitr.dsalab.a1;
 import java.util.Deque;
 import java.util.ArrayDeque;
 
-
 public class BracketsBalancer {
-	
+
 	public BracketsBalancer() {
-		
+
 	}
-	
+
 	/*
-	 
-	Input String : ([{}])
-	Result : Has Balanced Brackets
+	 * 
+	 * Input String : ([{}]) Result : Has Balanced Brackets
+	 * 
+	 * Input String : [()]{}{[()()]()} Result : Has Balanced Brackets
+	 * 
+	 * Input String : [()]{}{[)()]()} Result : Do not contain Balanced Brackets
+	 * 
+	 * Input String : {{{{{{{[[[[[[]]]]]((((((()))))))}}}}}} Result : Do not contain
+	 * Balanced Brackets
+	 * 
+	 * Input String : c{[(a)b]} Result : Has Balanced Brackets
+	 * 
+	 */
 
-	Input String : [()]{}{[()()]()}
-	Result : Has Balanced Brackets
+	public static boolean isBalancedBracketsExpression(String inputString) {
 
-	Input String : [()]{}{[)()]()}
-	Result : Do not contain Balanced Brackets
-
-	Input String : {{{{{{{[[[[[[]]]]]((((((()))))))}}}}}}
-	Result : Do not contain Balanced Brackets
-
-	Input String : c{[(a)b]}
-	Result : Has Balanced Brackets
-			 
-	*/
-	
-	public static boolean isBalancedBracketsExpression(String inputString)  {
-		
-		if(inputString == null || inputString.length() == 0) {
+		if (inputString == null || inputString.length() == 0) {
 			return true;
 		}
-		
+
 		// Using ArrayDeque is faster than using Stack
 		Deque<Character> stack = new ArrayDeque<Character>();
 		int count = inputString.length();
 		int index = 0;
-		
+
 		for (; index < count; index++) {
-		
+
 			char c = inputString.charAt(index);
-			
-			if(c == '(' || c == '{' || c == '[') {
+
+			if (c == '(' || c == '{' || c == '[') {
 				stack.push(c);
-			}
-			else {
-				
+			} else {
+
 				if (c == ')' || c == '}' || c == ']') {
-					
+
 					if (stack.isEmpty()) {
 						break;
 					} else {
@@ -71,27 +65,22 @@ public class BracketsBalancer {
 				}
 			}
 		}
-		
-		if(index < count) {
+
+		if (index < count || !stack.isEmpty()) {
 			return false;
 		}
 		return true;
 	}
-	
+
 	public static void main(String[] args) {
-		
-		String exprList[] = {
-				"([{}])", 
-				"[()]{}{[()()]()}",  
-				"[()]{}{[)()]()}", 
-				"{{{{{{{[[[[[[]]]]]((((((()))))))}}}}}}",
-				"c{[(a)b]}"
-				};
-		
-		for(String expr: exprList) { 
-		
+
+		String exprList[] = { "([{}])", "[()]{}{[()()]()}", "[()]{}{[)()]()}", "{{{{{{{[[[[[[]]]]]((((((()))))))}}}}}}",
+				"c{[(a)b]}" };
+
+		for (String expr : exprList) {
+
 			System.out.println("\nInput String : " + expr);
-			
+
 			if (isBalancedBracketsExpression(expr)) {
 				System.out.println("Result : Has Balanced Brackets");
 			} else {
--- src/com/gl/iitr/dsalab/a2/TwoSumUsingBST.java
+++ src/com/gl/iitr/dsalab/a2/TwoSumUsingBST.java
@@ -9,113 +9,106 @@ public class TwoSumUsingBST {
 	static class Node {
 		int data;
 		Node left, right;
-		
-		Node(int data){
+
+		Node(int data) {
 			this.data = data;
 			this.left = null;
 			this.right = null;
 		}
 	}
-	
+
 	static Node newNode(int data) {
 		return new Node(data);
 	}
-	
+
 	static Node insert(Node root, int data) {
-		
-		if(root == null) {
+
+		if (root == null) {
 			return newNode(data);
-		}
-		else if(data < root.data){
+		} else if (data < root.data) {
 			root.left = insert(root.left, data);
-		}
-		else if(data > root.data){
+		} else if (data > root.data) {
 			root.right = insert(root.right, data);
 		}
 		return root;
 	}
-	
+
 	/*
-	 *  Output:
-	    Pair for sum 130 is ( 70, 60 )
-		Pair for sum 80 is ( 50, 30 )
-		Pair for sum 90 is ( 50, 40 )
-		Nodes pair not found for the sum: 200
-		Pair for sum 70 is ( 40, 30 )
+	 * Output: Pair for sum 130 is ( 70, 60 ) Pair for sum 80 is ( 50, 30 ) Pair for
+	 * sum 90 is ( 50, 40 ) Nodes pair not found for the sum: 200 Pair for sum 70 is
+	 * ( 40, 30 )
 	 */
-	
+
 	static boolean find2Sum(Node root, int sum, HashSet<Integer> hashSet) {
-		
-		if(root == null) {
+
+		if (root == null) {
 			return false;
 		}
-		
-		if(find2Sum(root.left, sum, hashSet)) {
+
+		if (find2Sum(root.left, sum, hashSet)) {
 			return true;
 		}
-		
-		if(hashSet.contains(sum - root.data)) {
-			System.out.println("Pair for sum " + sum + " is ( " + root.data + ", " + (sum-root.data) + " )");
+
+		if (hashSet.contains(sum - root.data)) {
+			System.out.println("Pair for sum " + sum + " is ( " + root.data + ", " + (sum - root.data) + " )");
 			return true;
 		}
-		
+
 		hashSet.add(root.data);
-		
-		if(find2Sum(root.right, sum, hashSet)) {
+
+		if (find2Sum(root.right, sum, hashSet)) {
 			return true;
 		}
-		
+
 		return false;
 	}
-	
+
 	static boolean findPairForSum(Node root, int sum) {
-		
+
 		HashSet<Integer> set = new HashSet<Integer>();
 		return find2Sum(root, sum, set);
 	}
-	
+
 	static void inorderTravel(Node root, LinkedList<Integer> list) {
-		
-		if(root == null) {
+
+		if (root == null) {
 			return;
 		}
-		
+
 		inorderTravel(root.left, list);
 		list.addLast(root.data);
 		inorderTravel(root.right, list);
 	}
-	
+
 	static int findHeightOfBST(Node root) {
-		
-		if(root == null) {
+
+		if (root == null) {
 			return 0;
 		}
-		
-		int leftHeight = 0, rightHeight = 0;  
-		
-		if(root.left != null) {
+
+		int leftHeight = 0, rightHeight = 0;
+
+		if (root.left != null) {
 			leftHeight = findHeightOfBST(root.left);
 		}
-		
-		if(root.right != null) {
+
+		if (root.right != null) {
 			rightHeight = findHeightOfBST(root.right);
 		}
-		
-		
-		int h = (leftHeight > rightHeight) ?leftHeight : rightHeight;
-		
-		return h+1;
+
+		int h = (leftHeight > rightHeight) ? leftHeight : rightHeight;
+
+		return h + 1;
 	}
-	
+
 	public TwoSumUsingBST() {
-		
-		
+
 	}
 
 	public static void main(String[] args) {
-		
+
 		Node root = null;
-		
+
 		root = insert(root, 40);
 		root = insert(root, 20);
 		root = insert(root, 10);
@@ -123,25 +116,24 @@ public class TwoSumUsingBST {
 		root = insert(root, 60);
 		root = insert(root, 50);
 		root = insert(root, 70);
-		
+
 		LinkedList<Integer> list = new LinkedList<Integer>();
 		inorderTravel(root, list);
-		
+
 		System.out.println("---- Inorder Traversal of constructed tree \n");
-		
+
 		Iterator<Integer> it = list.iterator();
-		while(it.hasNext()) {
+		while (it.hasNext()) {
 			System.out.print(it.next() + " ");
 		}
-		
+
 		System.out.println(" ---- Finding 2sum pairs ----- \n");
-		int sumList[] = {130, 80, 90, 200, 70};
-		
+		int sumList[] = { 130, 80, 90, 200, 70 };
+
 		for (int sum : sumList) {
 			if (!findPairForSum(root, sum)) {
 				System.out.println("Nodes pair not found for the sum: " + sum);
 			}
 		}
 	}
-
 }
